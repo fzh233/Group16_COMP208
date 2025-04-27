cc420bd53217e107b76adcb8915163d4d4b7a461
fzh233 <msfzh1296763208@outlook.com>
2025-04-27 05:19:28 +0100

    Delete code directory

--- code/firebase_config.py
+++
@@ -1,9 +0,0 @@
-firebase_config = {
-    "apiKey": "AIzaSyCj-dXCcRZzOaY35XV450VcfoCYlQdO-g4",
-    "authDomain": "stardew.firebaseapp.com",
-    "databaseURL": "https://stardew-9c37a-default-rtdb.europe-west1.firebasedatabase.app/",
-    "projectId": "stardew-9c37a",
-    "storageBucket": "stardew-9c37a.firebasestorage.app",
-    "messagingSenderId": "729545882521",
-    "appId": "1:729545882521:web:ca29bcd768b29fe4b9f108"
-}
